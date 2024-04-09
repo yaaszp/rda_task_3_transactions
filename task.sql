76828c2e5773ef77235f4645e16345f90a4dc2b8
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-04-09 18:40:41 +0300

    fixed error from bash tests

--- task.sql
+++ task.sql
@@ -3,10 +3,9 @@ USE ShopDB;
 INSERT INTO Orders (CustomerID, Date)
 	VALUES (1, '2024-04-09');
 
+START TRANSACTION;
 INSERT INTO OrderItems (OrderID, ProductID, Count)
 	VALUES (1, 1, 0);
-
-START TRANSACTION; 
 UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = '1'; 
 UPDATE OrderItems SET Count = Count + 1 WHERE ID = '1';
 
